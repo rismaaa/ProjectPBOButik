d6cf8b40f98e954092e37fcff400c4bcd5b9fb38
rismaaa <rrisma334@gmail.com>
2022-01-12 11:42:58 +0700

    ProjectPBOButik

---
+++ Tugas Project Sistem Butik/Tugas Project Sistem Butik/src/Tugas_Kelompok_PBO_Malam/Main_Penjualan.java
@@ -0,0 +1,9 @@
+package Tugas_Kelompok_PBO_Malam;
+
+
+public class Main_Penjualan {
+    public static void main(String[] args) {
+        perhitunganpenjualan Penjualan = new perhitunganpenjualan();
+        Penjualan.setVisible(true);
+    }
+}
